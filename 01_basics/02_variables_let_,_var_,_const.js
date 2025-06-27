fe449759531a202c6d1fef900e52e2903690dbae
Ahmed-Coder496 <zai015693@gmail.com>
2025-06-27 21:30:44 +0000

    variables file

---
+++ 01_basics/02_variables_let_,_var_,_const.js
@@ -0,0 +1,15 @@
+// VARIABLES
+
+const userName = "Ahmed"
+let userId = "555"
+var userPw = 321
+userIdentity = "Pakistan"
+
+// userName = "Ali" // It is not allowed
+userId = 123
+userPw = "123"
+userIdentity = "Karachi"
+
+console.table([userName, userId, userPw, userIdentity]);
+
+// Kindly don't use var
