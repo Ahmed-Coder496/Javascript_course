b5ba8901af7a329fe1a5dd0b43361d51664fd5ce
Ahmed-Coder496 <zai015693@gmail.com>
2025-06-28 19:09:06 +0000

    we study datatypes

---
+++ 01_basics/03_datatypes_and_ECMA_standard.js
@@ -0,0 +1,19 @@
+"use strict" // treet all js code in latest version
+
+//alert(3+3) //we are not work in browser
+
+console.log(3
+    +
+    3);
+console.log("Ahmed"); // code read ability is high
+
+//DATATYPES
+/*
+string => ""
+number => 123
+boolean => true/false
+Null => stndalone value
+undefined => undefined
+bigInt => 
+Symbol => unique
+*/
