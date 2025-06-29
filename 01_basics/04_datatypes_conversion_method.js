a72633f57d9724fc93ff5cbe198cde247d8b25f2
Ahmed_Zai <zai015693@gmail.com>
2025-06-29 19:17:21 +0000

    datatypes_conversion_method

---
+++ 01_basics/04_datatypes_conversion_method.js
@@ -0,0 +1,33 @@
+// DATATYPES CONVERSION
+
+// For Numer
+let userId = "555"
+// console.log(userId);
+// console.log(typeof(userId))
+let numberUserId = Number(userId)
+console.log(numberUserId);
+console.log(typeof(numberUserId))
+
+let AhmedId = "555"
+// console.log(AhmedId);
+// console.log(typeof(AhmedId))
+let AhmedPw = Number(AhmedId)
+// console.log(AhmedPw);
+// console.log(typeof(AhmedPw))
+
+
+// For String
+let aliId = 666
+// console.log(aliId);
+// console.log(typeof(aliId))
+let aliPw = String(aliId)
+// console.log(aliPw);
+// console.log(typeof(aliPw))
+
+// For Boolean
+let ayanId = 111
+// console.log(ayanId);
+// console.log(typeof(ayanId))
+let ayanPw = String(ayanId)
+// console.log(ayanPw);
+// console.log(typeof(ayanPw))
