257c0883652bd4c6607280dd8879ea0191bb99ce
Ahmed_Zai <zai015693@gmail.com>
2025-07-01 20:11:14 +0000

    Operation file done

---
+++ 01_basics/05_operations.js
@@ -0,0 +1,27 @@
+"use strict"
+
+
+// OPERATIONS
+
+// console.log(3 + 3);    // => 6
+// console.log(3 - 3);    // => 0
+// console.log(3 * 3);    // => 9
+// console.log(3 % 3);    // => 0
+// console.log("3" + 3);    // => 33
+// console.log(3 + "3");    // => 33
+// console.log("3" + 3 + 3);    // => 333
+// console.log(3 + "3" + 3);    // => 333
+// console.log(3 + 3 + "3");    // => 63
+// console.log("Ahmed" + " Afaq");    // => Ahmed Afaq
+// console.log(true);    // => true
+// console.log(+true);    // => 1
+// console.log(-true);    // => -1
+// console.log(false);    // => false
+// console.log(+false);    // => 0
+// console.log(-false);    // => -0
+
+let gameCounter = 100
+// gameCounter++;     // this is postfix => 101 
+// ++gameCounter;     // this is prefix => 101 
+console.log(gameCounter);
+
