44c4843df734a1907d562aaee22254d933377af8
Ahmed_Zai <zai015693@gmail.com>
2025-07-01 18:42:40 +0000

    done this file

--- 01_basics/04_datatypes_conversion_method.js
+++ 01_basics/04_datatypes_conversion_method.js
@@ -1,33 +1,84 @@
 // DATATYPES CONVERSION
 
+
 // For Numer
 let userId = "555"
-// console.log(userId);
-// console.log(typeof(userId))
+// console.log(userId);     //=> 555
+// console.log(typeof(userId))     //=> string
 let numberUserId = Number(userId)
-console.log(numberUserId);
-console.log(typeof(numberUserId))
+// console.log(numberUserId);     //=> 555
+// console.log(typeof(numberUserId))     //=> number
 
-let AhmedId = "555"
-// console.log(AhmedId);
-// console.log(typeof(AhmedId))
+let AhmedId = "555abc"
+// console.log(AhmedId);     //=> 555abc
+// console.log(typeof(AhmedId))     //=> string
 let AhmedPw = Number(AhmedId)
-// console.log(AhmedPw);
-// console.log(typeof(AhmedPw))
+// console.log(AhmedPw);     //=> NaN
+// console.log(typeof(AhmedPw))     //=> number
+
+let ayazId = false
+// console.log(ayazId);     //=> false
+// console.log(typeof(ayazId))     //=> boolean
+let ayazPw = Number(ayazId)
+// console.log(ayazPw);     //=> 0
+// console.log(typeof(ayazPw))     //=> number
+
+let aiyshaId = true
+// console.log(aiyshaId);     //=> true
+// console.log(typeof(aiyshaId))     //=> boolean
+let aiyshaPw = Number(aiyshaId)
+// console.log(aiyshaPw);     //=> 1
+// console.log(typeof(aiyshaPw))     //=> number
 
 
 // For String
 let aliId = 666
-// console.log(aliId);
-// console.log(typeof(aliId))
+// console.log(aliId);     //=> 666
+// console.log(typeof(aliId))     //=> number
 let aliPw = String(aliId)
-// console.log(aliPw);
-// console.log(typeof(aliPw))
+// console.log(aliPw);     //=> 666
+// console.log(typeof(aliPw))     //=> string
+
+let mehekId = "666abc"
+// console.log(mehekId);     //=> 666abc
+// console.log(typeof(mehekId))     //=> string
+let mehekPw = String(mehekId)
+// console.log(mehekPw);     //=> 666abc
+// console.log(typeof(mehekPw))     //=> string
+
+let fatimaId = true
+// console.log(fatimaId);     //=> true
+// console.log(typeof(fatimaId))     //=> boolean
+let fatimaPw = String(fatimaId)
+// console.log(fatimaPw);     //=> ture
+// console.log(typeof(fatimaPw))     //=> string
+
 
 // For Boolean
-let ayanId = 111
-// console.log(ayanId);
-// console.log(typeof(ayanId))
-let ayanPw = String(ayanId)
-// console.log(ayanPw);
-// console.log(typeof(ayanPw))
+let ayanId = 1
+// console.log(ayanId);     //=> 1
+// console.log(typeof(ayanId))     //=> number
+let ayanPw = Boolean(ayanId)
+// console.log(ayanPw);     //=> true
+// console.log(typeof(ayanPw))     //=> boolean
+
+let ebadId = 0
+// console.log(ebadId);     //=> 0
+// console.log(typeof(ebadId))     //=> number
+let ebadPw = Boolean(ebadId)
+// console.log(ebadPw);     //=> false
+// console.log(typeof(ebadPw))     //=> boolean
+
+let furqanId = "abc"
+// console.log(furqanId);     //=> abc
+// console.log(typeof(furqanId))     //=> string
+let furqanPw = Boolean(furqanId)
+// console.log(furqanPw);     //=> true
+// console.log(typeof(furqanPw))     //=> boolean
+
+let fahadId = ""
+// console.log(fahadId);     //=> 
+// console.log(typeof(fahadId))     //=> string
+let fahadPw = Boolean(fahadId)
+// console.log(fahadPw);     //=> false
+// console.log(typeof(fahadPw))     //=> boolean
