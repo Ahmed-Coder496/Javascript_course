e6be7edaef044655d2dded1b36c8f86381b62504
Ahmed_Zai <zai015693@gmail.com>
2025-07-16 22:19:26 +0000

    Strings

---
+++ 01_basics/09_strings_in_javascrpt.js
@@ -0,0 +1,27 @@
+"use_strict"
+
+// Strings
+
+const gameName = new String ("PubG")
+// console.log(gameName);        // => [String: 'PubG']
+
+let userName = "Ahmed"
+// console.log(userName);        // => Ahmed
+// console.log(userName.at(3));        // => e
+// console.log(userName.charAt(3));        // => e
+
+const icons = "☃";
+// console.log(icons.codePointAt(0));        // => 9731
+
+let suprise = " wow!"
+// console.log(`${suprise.repeat(3)} What's suprizing moment`);        // => wow! wow! wow! What's suprizing moment
+
+let replace = "you are a good footballer"
+// console.log(`${replace.replace("you are", "I'm")}`);        // => I'm a good footballer
+
+let replaceall = "This is farrari. farrari is the best car and the color of farrari is red"
+// console.log(`${replaceall.replaceAll("farrari", "Roles Royals")}`);        // => This is Roles Royals. Roles Royals is the best car and the color of Roles Royals is red
+
+
+
+
