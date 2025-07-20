ef8558fce336ae89474e4953fab50b10082e3bec
Ahmed_Zai <zai015693@gmail.com>
2025-07-20 20:31:43 +0000

    Numbers completed

--- 01_basics/10_numbers_in_javascript.js
+++ 01_basics/10_numbers_in_javascript.js
@@ -15,4 +15,24 @@ let toStringReplace = 2000
 // console.log(toString.toString().replace("000", "111"));        // => 2111
 
 let exponential = 234.324
-console.log(exponential.toExponential(3));
+// console.log(exponential.toExponential(4));        // => 2.3432e+2
+
+let precision = 987.324
+// console.log(precision.toPrecision(2));        // => 9.9e+2
+
+let valueOf = new Number (345)
+// console.log(valueOf);        // => [Number: 345]
+// console.log(valueOf.valueOf());        // => 345
+// console.log(typeof(valueOf.valueOf()));        // => number
+
+let fixed = 99.099
+// console.log(fixed.toFixed(2));        // => 99.10
+// console.log(fixed.toFixed(1));        // => 99.1
+
+let toLocalString = 1234567890
+// console.log(toLocalString.toLocaleString("en-PK"));        // => 1,234,567,890
+// console.log(toLocalString.toLocaleString("en-EG"));        // => 1,234,567,890
+// console.log(toLocalString.toLocaleString("en-IN"));        // => 1,23,45,67,890
+
+
+
