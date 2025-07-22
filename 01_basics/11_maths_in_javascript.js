377a15e39f159b587b4f0e509222e64183fb3b81
Ahmed_Zai <zai015693@gmail.com>
2025-07-22 01:08:13 +0000

    Maths

---
+++ 01_basics/11_maths_in_javascript.js
@@ -0,0 +1,33 @@
+"use_strict"
+
+// Maths
+
+let absolute = -4
+// console.log(Math.abs(absolute));        // => 4
+
+let round = 123.956
+// console.log(Math.round(round));        // => 124
+
+let ceil = 1.2
+// console.log(Math.ceil(ceil));        // => 2
+
+let floor = 1.2
+// console.log(Math.floor(floor));        // => 1
+
+// console.log(Math.min(2, 3, 6, 1, 5));        // => 1
+
+// console.log(Math.max(2, 3, 6, 1, 5));        // => 6
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+let min = 20
+let max = 40
+
+console.log(Math.floor(Math.random() * (max - min) + min));
+
+
+
+
