873594d2622ced0a475553ba28b0b0bfd5cc1439
Ahmed_Zai <zai015693@gmail.com>
2025-07-28 18:58:39 +0000

    ..

--- 01_basics/13_arrays.js
+++ 01_basics/13_arrays.js
@@ -3,30 +3,37 @@
 // arrays
 
 let strArr = ["Ahmed", "Rao_Haseeb", "Shahwaiz"]
-console.log(strArr);        // => [ 'Ahmed', 'Rao_Haseeb', 'Shahwaiz' ]
+// console.log(strArr);        // => [ 'Ahmed', 'Rao_Haseeb', 'Shahwaiz' ]
 
 let numArr = [0, 1]
-console.log(numArr.length);        // => 2
-console.log(numArr);        // => [ 0, 1 ]
+// console.log(numArr.length);        // => 2
+// console.log(numArr);        // => [ 0, 1 ]
 
 // Arr properties
 
 // numArr
 numArr.push(2)
-console.log(numArr);        // => [ 0, 1, 2 ]
+// console.log(numArr);        // => [ 0, 1, 2 ]
 numArr.pop()
-console.log(numArr);        // => [ 0, 1 ]
+// console.log(numArr);        // => [ 0, 1 ]
 
 // strArr
 strArr.unshift("Ali")
-console.log(strArr);        // => [ 'Ali', 'Ahmed', 'Rao_Haseeb', 'Shahwaiz' ]
+// console.log(strArr);        // => [ 'Ali', 'Ahmed', 'Rao_Haseeb', 'Shahwaiz' ]
 strArr.shift("Ali")
-console.log(strArr);        // => [ 'Ahmed', 'Rao_Haseeb', 'Shahwaiz' ]
+// console.log(strArr);        // => [ 'Ahmed', 'Rao_Haseeb', 'Shahwaiz' ]
 
 
-console.log(numArr.includes(1));        // => true
-console.log(numArr.indexOf(1));        // => 1
-console.log(numArr.indexOf(2));        // => -1 // bsc those value whose does'nt exist in array 
-console.log(numArr.join());        // => -1 // it convert array into string
+// console.log(numArr.includes(1));        // => true
+// console.log(numArr.indexOf(1));        // => 1
+// console.log(numArr.indexOf(2));        // => -1 // bsc those value whose does'nt exist in array 
+// console.log(numArr.join());        // => -1 // it convert array into string
+
+let cricketTeam = ["Babar", "Shaheen-Shah", "Shuaib-Akhtar"]
+
+let footBallTeam = ["Ronaldo", "Messi", "Erling Haaland"]
+
+
+console.log(cricketTeam.concat(footBallTeam));
 
 
