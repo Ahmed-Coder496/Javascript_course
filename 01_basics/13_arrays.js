e7ec7af8df32f1b09c2c2acdde88d5244e43312e
Ahmed_Zai <zai015693@gmail.com>
2025-07-29 20:12:30 +0000

    Arrays completed

--- 01_basics/13_arrays.js
+++ 01_basics/13_arrays.js
@@ -29,11 +29,30 @@ strArr.shift("Ali")
 // console.log(numArr.indexOf(2));        // => -1 // bsc those value whose does'nt exist in array 
 // console.log(numArr.join());        // => -1 // it convert array into string
 
-let cricketTeam = ["Babar", "Shaheen-Shah", "Shuaib-Akhtar"]
+let cricketTeam = ["Babar-Azam", "Shaheen-Shah", "Shuaib-Akhtar"]
 
 let footBallTeam = ["Ronaldo", "Messi", "Erling Haaland"]
 
+// const gameTeam = cricketTeam.concat(footBallTeam)           // => kindly don't use concat method in modern way
 
-console.log(cricketTeam.concat(footBallTeam));
+const gameTeam = [...cricketTeam, ...footBallTeam]
+
+// console.log(gameTeam);        // => ['Babar-Azam','Shaheen-Shah','Shuaib-Akhtar','Ronaldo','Messi','Erling Haaland']
+
+let difArr = [1, 2, 3, [4, 5, 6, [1, 2, 7], 4], 3]
+
+let newDifArr = difArr.flat(1)
+// console.log(newDifArr);        // => [ 1, 2, 3, 4, 5, 6, [ 1, 2, 7 ], 4, 3 ]
+let firDifArr = difArr.flat(Infinity)
+// console.log(firDifArr);        // => [1, 2, 3, 4, 5,6, 1, 2, 7, 4,3]
+
+console.log(Array.isArray("Ahmed"));        // => false
+console.log(Array.from("Ahmed"));        // => [ 'A', 'h', 'm', 'e', 'd' ]
+let firArr = 1
+let secArr = 2
+let thirArr = 3
+
+console.log(Array.of(firArr, secArr, thirArr));
+
 
 
