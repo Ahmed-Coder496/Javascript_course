8c8a735529c60cba18a110ced4353c90cf584204
Ahmed_Zai <zai015693@gmail.com>
2025-08-02 22:43:25 +0000

    ...

--- 01_basics/14_objects.js
+++ 01_basics/14_objects.js
@@ -2,27 +2,60 @@
 
 // Objects
 
+// Singleton Objects
+
+let newObj = new Object()
+// console.log(newObj);        // => {}
+
+
 // Object Literals
 
 let idNum = Symbol()
 let myObj = {
-    name: "Ahmed",
-    age : 18,
-    email:"abc123@gmail.com",
-    [idNum]:"969",
-    idNum:969,
+  name: "Ahmed",
+  age: 18,
+  email: "abc123@gmail.com",
+  [idNum]: "969",
+  idNum: 969,
 }
-console.log(myObj);
-console.log(myObj.age);
-console.log(myObj["name"]);
-console.log(myObj.email);
+// console.log(myObj);        // => {name: 'Ahmed',age: 18,email: 'abc123@gmail.com',idNum: 969,[Symbol()]: '969'}
+// console.log(myObj.age);        // => 18
+// console.log(myObj["name"]);        // => Ahmed
+// console.log(myObj.email);        // => abc123@gmail.com
 myObj.email = "cba321@gmail.com"
 // Object.freeze(myObj)
 myObj.email = "email321@gmail.com"
-console.log(myObj.email);
+// console.log(myObj.email);        // => email321@gmail.com
 
-myObj.greeting = function(){
-    console.log(`Hi, ${this.name}`);
+myObj.greeting = function () {
+  // console.log(`Hi, ${this.name}`);        // => Hi, Ahmed
 }
-console.log(myObj.greeting());
+// console.log(myObj.greeting());        // => undefined
+
+let userOne = {
+  name: "Haseeb",
+  email: "haseeb@gmail.com",
+  age: 16
+}
+
+let userTwo = {
+  name: "Ali",
+  email: "ali@gmail.com",
+  id: 17
+}
+
+let userThird = {...userOne, ...userTwo}
+
+console.log(userThird);        // => { name: 'Ali', email: 'ali@gmail.com', age: 16, id: 17 }                 // => Don't use this method in these days
+
+
+
+
+
+
+
+
+
+
+
 
