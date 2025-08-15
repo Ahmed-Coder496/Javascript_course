601093eafdd83150d98b567f2ac77f94beee4170
Ahmed_Zai <zai015693@gmail.com>
2025-08-15 13:31:39 +0000

    ..

---
+++ 01_basics/functions/01_functions.js
@@ -0,0 +1,10 @@
+"use_strict"
+
+//Functions :
+
+function func(num1, num2) {
+    return 2 + 2
+}
+func()        // => It call's the function
+console.log(func);        // => It gives function reference
+console.log(func());        // => 4
