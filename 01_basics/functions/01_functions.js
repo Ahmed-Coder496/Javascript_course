cca649739cc497a10dbc89013498a24bba3930d2
Ahmed_Zai <zai015693@gmail.com>
2025-08-16 02:48:24 +0000

    ..

--- 01_basics/functions/01_functions.js
+++ 01_basics/functions/01_functions.js
@@ -2,9 +2,30 @@
 
 //Functions :
 
+// 01_function
 function func(num1, num2) {
-    return 2 + 2
+    return num1 + num2
 }
 func()        // => It call's the function
 console.log(func);        // => It gives function reference
-console.log(func());        // => 4
+console.log(func(2, 4));        // => 6
+
+// 02_functions
+function nextFunc(num1, num2) {
+    let sum = num1 * num2
+    return sum
+}
+func()        // => It call's the function
+console.log(nextFunc);        // => It gives function reference
+console.log(nextFunc(2, 4));        // => 6
+
+// 03_functions
+function newFunc(num1, num2) {
+    let sum = num1 * num2
+    return sum
+}
+func()        // => It call's the function
+console.log(newFunc);        // => It gives function reference
+console.log(newFunc(2, 4));        // => 6
+
+
