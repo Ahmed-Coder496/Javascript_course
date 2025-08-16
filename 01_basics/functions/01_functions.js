db03a3a095676a0ad0c3c2f8208d86cf8109e9c0
Ahmed_Zai <zai015693@gmail.com>
2025-08-16 10:34:49 +0000

    ..

--- 01_basics/functions/01_functions.js
+++ 01_basics/functions/01_functions.js
@@ -7,8 +7,8 @@ function func(num1, num2) {
     return num1 + num2
 }
 func()        // => It call's the function
-console.log(func);        // => It gives function reference
-console.log(func(2, 4));        // => 6
+// console.log(func);        // => It gives function reference
+// console.log(func(2, 4));        // => 6
 
 // 02_functions
 function nextFunc(num1, num2) {
@@ -16,16 +16,16 @@ function nextFunc(num1, num2) {
     return sum
 }
 func()        // => It call's the function
-console.log(nextFunc);        // => It gives function reference
-console.log(nextFunc(2, 4));        // => 6
+// console.log(nextFunc);        // => It gives function reference
+// console.log(nextFunc(2, 4));        // => 6
 
 // 03_functions
-function newFunc(num1, num2) {
-    let sum = num1 * num2
-    return sum
+function newFunc(userName) {
+    return `My name is ${userName}.`
 }
 func()        // => It call's the function
-console.log(newFunc);        // => It gives function reference
-console.log(newFunc(2, 4));        // => 6
+// console.log(newFunc);        // => It gives function reference
+// console.log(newFunc());        // => My name is undefined.
+// console.log(newFunc("Ahmed"));        // => My name is Ahmed.
 
 
