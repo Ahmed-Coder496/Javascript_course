8b6ac3c362087dade05982e957d9c797e27b2e90
Ahmed_Zai <zai015693@gmail.com>
2025-08-20 22:26:16 +0000

    ....

---
+++ 01_basics/functions/06_nested_function_,_if_...else.js
@@ -0,0 +1,22 @@
+"use_strict"
+
+// Nested Function in js
+
+function perentFunc(){
+    let perentFuncName = "Hen"
+    function childFunc (){
+        let childFuncName = "Chick"
+        console.log(perentFuncName);
+    }
+    // console.log(childFuncName);
+    childFunc()
+}
+perentFunc()
+
+// Nested if,...else in js
+
+if (true){
+    
+}
+
+
