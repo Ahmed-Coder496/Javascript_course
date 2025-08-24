1dca6985a762720fe8e8229c65275ce8d620d529
Ahmed_Zai <zai015693@gmail.com>
2025-08-24 13:58:33 +0000

    ...

--- 01_basics/functions/07_arrow_function_or_this._method.js
+++ 01_basics/functions/07_arrow_function_or_this._method.js
@@ -16,12 +16,33 @@ let user = {
 user.studentMessege()
 user.teacher = "Rao Haseeb"
 // user.studentMessege()
-console.log(this);
+// console.log(this);
 
 
+// Function is for reverse string output
+
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+console.log(reverseString("Are you crazy"));
+
+// Function is for count characs
+
+function characsount(str) {
+    return str.length
+}
+console.log(characsount("Are you crazy"));
+
+// Function is for max num
+
+function maxNum(arr) {
+    if (Math.max(...arr) > 99) {
+        console.log(`Don't enter three digit number`);
+
+    } else {
+        console.log(`${Math.max(...arr)}`);
+    }
+}
+console.log(maxNum([1, 3, 4, 6, 8]));
 
 
-
-
-let user = "Ahmed"
-function userReverse (){}
