59517b3f932f5eb63e1b9370f74a57ec35c56cea
Ahmed_Zai <zai015693@gmail.com>
2025-08-30 18:45:51 +0000

    ...

---
+++ 01_basics/01_functions/07_arrow_function_or_this._method.js
@@ -0,0 +1,64 @@
+"use_strict"
+
+// This method
+
+let user = {
+    teacher: "Sir Ahmed",
+    Gender: "Male",
+    course: "Web develpment",
+    studentMessege: function () {
+        // console.log(`Welcome to ${this.course}. Teached by ${this.teacher}.`);
+        // console.log(this);
+    }
+}
+
+// console.log(user);
+user.studentMessege()
+user.teacher = "Rao Haseeb"
+// user.studentMessege()
+// console.log(this);
+
+
+// Function is for reverse string output
+
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+console.log(reverseString("Are you crazy"));
+
+// Function is for count characs
+
+function characsount(str) {
+    return str.length
+}
+console.log(characsount("Are you crazy"));
+
+// Function is for max num
+
+function maxNum(arr) {
+    if (Math.max(...arr) > 99) {
+        console.log(`Don't enter three digit number`);
+
+    } else {
+        console.log(`${Math.max(...arr)}`);
+    }
+}
+console.log(maxNum([1, 3, 4, 6, 8]));
+
+
+// Arrow Functions
+
+// Syntax
+//     ()=> { }
+
+let ArrowFunc0 = (num1, num2) => {
+    return num1 - num2
+}
+
+console.log(ArrowFunc0(2, 3));
+
+let ArrowFunc1 = (value1, value2) => (value1 + value2)
+console.log(ArrowFunc1(4, 5));
+
+
+
