f4c8a9cf911418db93a4c75a4d6837f9a9772f94
Ahmed_Zai <zai015693@gmail.com>
2025-09-01 22:42:52 +0000

    ...

--- 01_basics/03_iterations_or_loops/04_for_in_loops.js
+++ 01_basics/03_iterations_or_loops/04_for_in_loops.js
@@ -2,11 +2,22 @@
 
 // for_in loops
 
-let object = {
+let object1 = {
     name: "Ahmed",
-    age: 12,
+    // age: 12,
     phoneNum: 123467899
+
 }
-for (const obj in object) {
-    console.log(`${obj} : ${object[obj]}`);
+
+let object2 = {
+    name: "Haseeb",
+    age: 13,
+    phoneNum: 9876543210
+}
+for (const obj in object1) {
+    // console.log(`${obj} : ${object[obj]}`);
+}
+
+for (const obj in object1 , object2) {
+    console.log(`Hi, ${object1.name} and your phone Num is ${object1.phoneNum} and Hi, ${object2.name} and your phone Num is ${object2.phoneNum} `);
 }
